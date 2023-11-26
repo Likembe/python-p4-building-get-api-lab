6dae59e6996645195a6b067ba728603f2e5a3735
Alpha <alpha.likembe@student.moringaschool.com>
2023-11-26 16:36:42 +0300

    Complete

--- server/app.py
+++ server/app.py
@@ -20,19 +20,51 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    data = [{'id': bakery.id, 'name': bakery.name, 'created_at': bakery.created_at} for bakery in bakeries]
+    return jsonify(data)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.get(id)
+    if bakery:
+        data = {
+            'id': bakery.id,
+            'name': bakery.name,
+            'created_at': bakery.created_at
+        } 
+        
+        return jsonify(data), 200
+    else:
+        return jsonify({'error': 'Bakery not found'}), 404
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    data = [
+        {
+            'id': bg.id, 
+            'name': bg.name, 
+            'price': bg.price,
+            'created_at': bg.created_at  # Include 'created_at' field
+        } 
+        for bg in baked_goods
+    ]
+    return jsonify(data), 200
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if most_expensive:
+        data = {
+            'id': most_expensive.id, 
+            'name': most_expensive.name, 
+            'price': most_expensive.price,
+            'created_at': most_expensive.created_at  # Include 'created_at' field
+        }
+        return jsonify(data), 200
+    else:
+        return jsonify({'error': 'No baked goods found'}), 404    
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
